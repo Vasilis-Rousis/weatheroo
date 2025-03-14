dba1d6464515b1797ba4fc95bb99f2d39c2d20ea
Vasilis-Rousis <vasilisrousis1998@gmail.com>
2025-03-14 22:36:41 +0200

    Added tailwind and shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ]
 })
